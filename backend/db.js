2c41e8e834572f520196f519938901b95a78aba1
osama seid <osaseid1@gmail.com>
2025-08-26 15:30:39 +0300

    updated

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise';
 const connection = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '0924475152@os', // Replace with your MySQL password
+    password: '', // Replace with your MySQL password
     database: 'book_store'
 });
 
